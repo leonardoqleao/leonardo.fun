e4bc60a43200ec69fadde4363c2ba922794a0e51
leonardo q <leonardoq.leao@outlook.com>
2024-02-09 13:11:21 -0300

    feat: struturing home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
 import Image from "next/image";
 import '@/app/layout.scss'
+import Header from "@/components/header/page"
+
 export default function Home() {
-  return (
-   <main className="main">
-    <body className="body">
-        
-    </body>
-   </main>
-  );
+    return (
+        <html className="scroll-smooth w-auto">
+            <body className="body max-w-screen-2xl bg-gray-700">
+                {/* <Header /> */}
+                <main className="main">
+                    <section className="h-screen overflow-hidden bg-gray-100  lg:overflow-hidden">
+                        <div className="h-screen mx-auto max-w-screen-2xl lg:px-8">
+                            <h1 className="text-black">Teeste</h1>
+                        </div>
+                    </section>
+                </main>
+            </body>
+        </html>
+    );
 }
---
+++ src/components/header/page.tsx
@@ -0,0 +1,9 @@
+export default function Header(): JSX.Element {
+    return (
+        <header className="navbar_root max-w-screen-2xl">
+            <nav className="text-black max-w-screen-2xl">
+                This is the 
+            </nav>
+        </header>
+    )
+}
