3c75239a41aa971e2e66c0feb83a6616441f7e29
leonardo q <leonardoq.leao@outlook.com>
2024-02-13 09:14:07 -0300

    feat: add hr line in footer component

--- src/components/footer/page.tsx
+++ src/components/footer/page.tsx
@@ -1,7 +1,8 @@
 
 export default function Footer(): JSX.Element {
     return (
-        <footer >
+        <footer>
+            <hr className="max-w-7xl mx-auto border-slate-600 h-1" />
             <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
                 <div className="flex justify-center space-x-6 md:order-2">
                     {navigation.map((item) => (
